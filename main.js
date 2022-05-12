9868e4ad1d3c7c683384d37e014112436335bced
CONVIDADO UNIFESO <CONVIDADO@acad.unifeso.lan>
2022-05-12 19:55:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function maior(){
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+    var num3 = parseInt(document.getElementById('num3').value);
+
+    if(num1 > num2 && num1 > num3){
+        alert(`O primeiro valor ${num1} é o maior`)
+    } else if( num2 > num1 && num2 > num3){
+        alert(`O segundo valor ${num2} é o maior`)
+    }else{
+        alert(`O terceiro valor ${num3} é o maior`)
+    }
+
+}
