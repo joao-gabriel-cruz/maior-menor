5dea9066b8fa05b71108d933e82c2b9a22f37624
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-13 16:16:59 -0300

    correção de bug na verificação dos numeros

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ function maior() {
   var num2 = parseFloat(document.getElementById('num2').value);
   var num3 = parseFloat(document.getElementById('num3').value);
 
-  if (num1 > num2 && num1 > num3) {
-    alert(`O primeiro valor ${num1} é o maior`);
-  } else if (num2 > num1 && num2 > num3) {
-    alert(`O segundo valor ${num2} é o maior`);
-  } else {
-    alert(`O terceiro valor ${num3} é o maior`);
+  if (num1 >= num2 && num1 >= num3) {
+    alert(`O valor ${num1} é o maior`);
+  } else if (num2 >= num1 && num2 >= num3) {
+    alert(`O valor ${num2} é o maior`);
+  } else if (num3 >= num1 && num3 >= num2) {
+    alert(`O valor ${num3} é o maior`);
   }
 }
